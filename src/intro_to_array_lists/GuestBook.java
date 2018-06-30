35253cf8a6ab2e81e9a839cc56d4aa44ea373ce4
codingpanda23 <league@WTS22.attlocal.net>
2018-06-30 15:58:40 -0700

    Guest Book
    I finished

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -9,17 +9,17 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook implements ActionListener{
-		JPanel panel = new JPanel();
-		JFrame frame = new JFrame();
-		JButton button = new JButton();
-		JButton button1 = new JButton();
-		
-		ArrayList <String> guests = new ArrayList <String> ();
-		
+public class GuestBook implements ActionListener {
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	JButton button = new JButton();
+	JButton button1 = new JButton();
+
+	ArrayList<String> guests = new ArrayList<String>();
+
 	public void createUI() {
-		
-		button.setText("Add Name");   
+
+		button.setText("Add Name");
 		button1.setText("View Names");
 		frame.add(panel);
 		panel.add(button);
@@ -28,40 +28,41 @@ public class GuestBook implements ActionListener{
 		frame.pack();
 		button.addActionListener(this);
 		button1.addActionListener(this);
-		
-		
+
 	}
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	public static void main(String[] args) {
 		GuestBook guestBook = new GuestBook();
-		guestBook.createUI(); 
-		
-		
-		
-		
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
-}
+		guestBook.createUI();
+
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource()==button) {
+		if (e.getSource() == button) {
 			String name = JOptionPane.showInputDialog("Give me a NAME:");
 			guests.add(name);
 		}
-		
-		if (e.getSource()==button1) {
-			JOptionPane.showMessageDialog(null, name);
-			for (int i=0; i<guests.size();i++) {
-				System.out.println(guests.get(i));
+
+		if (e.getSource() == button1) {
+			String people = "";
+			for (int i = 0; i < guests.size(); i++) {
+				people += "Guest #" + (i + 1) + ": " + guests.get(i) + "\n";
+
 			}
-			
-			
+			JOptionPane.showMessageDialog(null, people);
 		}
-}
+	}
 }
